0755d173032f301a7fda5170bd79bf2f8bed265b
Leo Meira Vital 113110054 <leomv@lcc2-36.lcc.ufcg.edu.br>
2017-07-04 18:26:32 -0300

    Adicionando terceira questão incompleta (ordem do output não está correta)

---
+++ haskell/PyKTLlxK4.hs
@@ -0,0 +1,21 @@
+getFirstElement :: ([String], Int) -> [String]
+getFirstElement (first, second) = first
+
+getSecondElement :: ([String], Int) -> Int
+getSecondElement (first, second) = second
+
+doOp :: [String] -> [(String, Int)]
+doOp lista = doOpAux lista []
+
+doOpAux :: [String] -> [(String, Int)] -> [(String, Int)]
+doOpAux lista listaTupla = if length lista == 0 then listaTupla else doOpAux (getFirstElement (countAndFilter (head lista) lista)) listaTupla ++ [((head lista, (getSecondElement (countAndFilter (head lista) lista))))]
+
+countAndFilter :: String -> [String] -> ([String], Int)
+countAndFilter string lista = countAndFilterAux string lista ([], 0)
+
+countAndFilterAux :: String -> [String] -> ([String], Int) -> ([String], Int)
+countAndFilterAux string lista1 (lista2, count) = if (length lista1) == 0 then (lista2, count) else if string == head lista1 then countAndFilterAux string (tail lista1) (lista2, count+1) else countAndFilterAux string (tail lista1) (lista2 ++ [head lista1], count)
+
+main = do
+	lista <- getLine
+	print(doOp (read lista))
